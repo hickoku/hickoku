2f6317d2f46eef20b3dca6009a4f3f9c2f4697f8
Mohammed Sabir <62296993+msabirz@users.noreply.github.com>
2026-04-28 17:44:38 +0530

    stage-robot.txt

--- app/robots.ts
+++ app/robots.ts
@@ -4,12 +4,7 @@ export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: '*',
-      allow: '/',
-      disallow: [
-        '/admin/',
-        '/checkout/success',
-        '/checkout/confirmation',
-      ],
+      disallow: '/',
     },
     sitemap: 'https://www.hickoku.com/sitemap.xml',
     host: 'https://www.hickoku.com',
